93b568f73b0f41f6e06e11b34d46d6186b09e81f
Michael Lang <webdienste@h-da.de>
2022-09-28 21:29:48 +0200

    localconf geändert

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints'      => [
         [
             'news' => '10.0.0- ',
-            'typo3' => '10.4.0-10.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests'  => [],
