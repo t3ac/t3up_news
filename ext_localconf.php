bd9b816dc9ca54e663d1e4e68d0f8a82bf3751df
Michael Lang <webdienste@h-da.de>
2022-09-28 22:12:11 +0200

    t3ac in localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,7 @@ defined('TYPO3_MODE') or die();
 $boot = function () {
 
 	// Update flexforms
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]
-		   = \T3up\T3upNews\Hooks\FlexFormHook::class;
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][] = \T3ac\T3upNews\Hooks\FlexFormHook::class;
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/TCEFormNews.typoscript">');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TypoScript/setup.typoscript">');
