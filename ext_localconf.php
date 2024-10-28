d158f302e437c98bab99526e4b65072d89dc5378
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-10-28 07:06:54 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,22 +1,21 @@
 <?php
-defined('TYPO3_MODE') or die();
 
+defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(static function () {
-    
-    // Update flexforms
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][] = \T3ac\T3upNews\Hooks\FlexFormHook::class;
-    
-    ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Templates.tsconfig">');
-    ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig">');
+	
+	// Update flexforms
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][] = \T3ac\T3upNews\Hooks\FlexFormHook::class;
+	
+	ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Templates.tsconfig">');
+	ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig">');
 
-    if (ExtensionManagementUtility::isLoaded('eventnews')) {
-        ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/Eventnews/Templates.tsconfig">');
-        ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig">');
-        ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TypoScript/Eventnews/setup.typoscript">');
-        ExtensionManagementUtility::addTypoScriptConstants('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TypoScript/Eventnews/constants.typoscript">');
-    }
-    
+	if (ExtensionManagementUtility::isLoaded('eventnews')) {
+		ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/Eventnews/Templates.tsconfig">');
+		ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig">');
+		ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TypoScript/Eventnews/setup.typoscript">');
+		ExtensionManagementUtility::addTypoScriptConstants('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TypoScript/Eventnews/constants.typoscript">');
+	}
 	
 });
