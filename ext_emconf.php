84c97fd40bc12b0d60d9425ff6a61182708b0003
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-10-28 06:31:58 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title'            => 'T3UP - Newsextender',
     'description'      => 'Extends the tx_news Plugin with fields bootstrap-carousel and templates eventnews',
-    'version'          => '11.1.01',
+    'version'          => '12.0.01',
     'state'            => 'stable',
     'category'         => 'templates',
     'author'           => 'Michael Lang',
@@ -19,9 +19,9 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => false,
     'constraints'      => [
         'depends'   => [
-            'typo3' => '11.5.0-12.4.99',
-            'news' => '11.0-',
-            't3up' => '11.0-',
+            'typo3' => '12.4.0-',
+            'news' => '11.4-',
+            't3up' => '12.0-',
         ],
         'conflicts' => [],
         'suggests'  => [],
