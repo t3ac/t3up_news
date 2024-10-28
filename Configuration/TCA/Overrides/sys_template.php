1b21d800920f44b9df33b24a6fd4a0734f3c9ea4
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-10-28 06:38:33 +0100

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,3 @@
 <?php
-
-defined('TYPO3_MODE') or die();
-
+defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3up_news', 'Configuration/TypoScript/News', 'T3UP - News - Setup-Installation');
