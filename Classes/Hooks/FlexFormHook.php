084eb8aec210f9e91a05551213ac4a23daa29969
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-12-16 16:16:12 +0100

    Add files via upload

--- Classes/Hooks/FlexFormHook.php
+++ Classes/Hooks/FlexFormHook.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace T3ac\T3upNews\Hooks;
 
-use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Configuration\Event\AfterFlexFormDataStructureParsedEvent;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
-
 
 class FlexFormHook
 {
@@ -16,32 +15,30 @@ class FlexFormHook
    * @param array $identifier
    * @return array
    */
-   public function parseDataStructureByIdentifierPostProcess(array $dataStructure, array $identifier): array
-   {
+    public function __invoke(AfterFlexFormDataStructureParsedEvent $event): void
+    {
+        $dataStructure = $event->getDataStructure();
+        $identifier = $event->getIdentifier();
+        
+        if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $identifier['dataStructureKey'] === '*,news_newsliststicky') {
+            $file = GeneralUtility::getFileAbsFileName('EXT:t3up_news/Configuration/FlexForms/NewsList.xml');
+            $content = file_get_contents($file);
+            
+            if ($content) {
+                $dataStructure['sheets']['extraEntry'] = GeneralUtility::xml2array($content);
+            }
+        }
+        
+        if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $identifier['dataStructureKey'] === '*,news_newsdetail') {
+            $file = GeneralUtility::getFileAbsFileName('EXT:t3up_news/Configuration/FlexForms/NewsDetail.xml');
+            $content = file_get_contents($file);
+            
+            if ($content) {
+                $dataStructure['sheets']['extraEntry'] = GeneralUtility::xml2array($content);
+            }
+        }
+        
+         $event->setDataStructure($dataStructure);
+    }
 
-     if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $identifier['dataStructureKey'] === '*,news_newsliststicky') {
-         $file = GeneralUtility::getFileAbsFileName('EXT:t3up_news/Configuration/FlexForms/NewsList.xml');
-         $content = file_get_contents($file);
-         
-         if ($content) {
-             $dataStructure['sheets']['extraEntry'] = GeneralUtility::xml2array($content);
-         }
-     }
-     
-     if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $identifier['dataStructureKey'] === '*,news_newsdetail') {
-         $file = GeneralUtility::getFileAbsFileName('EXT:t3up_news/Configuration/FlexForms/NewsDetail.xml');
-         $content = file_get_contents($file);
-         
-         if ($content) {
-             $dataStructure['sheets']['extraEntry'] = GeneralUtility::xml2array($content);
-         }
-     }
-     
-     
-     return $dataStructure;
-   } 
-   
-   
 }
-
- 
