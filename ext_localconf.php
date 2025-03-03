2823801ca7e82b9696815b7dfe44faca68d1da63
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-03-03 20:37:49 +0100

    Add files via upload

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,23 +1,19 @@
 <?php
-
 defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(static function () {
-    
-    // Update flexforms
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][] = \T3ac\T3upNews\Hooks\FlexFormHook::class;
-    
-    ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Templates.tsconfig">');
-    ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig">');
-
-    if (ExtensionManagementUtility::isLoaded('eventnews')) {
-        ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/Eventnews/Templates.tsconfig">');
-        ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig">');
-        ExtensionManagementUtility::addTypoScriptSetup('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TypoScript/Eventnews/setup.typoscript">');
-        ExtensionManagementUtility::addTypoScriptConstants('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_news/Configuration/TypoScript/Eventnews/constants.typoscript">');
-    }
-    
 	
+	// Update flexforms
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][] = \T3ac\T3upNews\Hooks\FlexFormHook::class;
+	
+	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/News/Templates.tsconfig'");
+	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig'");
+
+	if (ExtensionManagementUtility::isLoaded('eventnews')) {
+        ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/Eventnews/Templates.tsconfig'");
+        ExtensionManagementUtility::addTypoScriptSetup("@import 'EXT:t3up_news/Configuration/TypoScript/Eventnews/setup.typoscript'");
+		ExtensionManagementUtility::addTypoScriptConstants("@import 'EXT:t3up_news/Configuration/TypoScript/Eventnews/constants.typoscript'");
+	}
 });
