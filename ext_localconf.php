35cb8b6e17335c9e274c35b12843505e6bda0606
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-03-03 21:03:23 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,7 +12,7 @@ call_user_func(static function () {
 	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig'");
 
 	if (ExtensionManagementUtility::isLoaded('eventnews')) {
-        ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/Eventnews/Templates.tsconfig'");
+       		ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/Eventnews/Templates.tsconfig'");
         	ExtensionManagementUtility::addTypoScriptSetup("@import 'EXT:t3up_news/Configuration/TypoScript/Eventnews/setup.typoscript'");
 		ExtensionManagementUtility::addTypoScriptConstants("@import 'EXT:t3up_news/Configuration/TypoScript/Eventnews/constants.typoscript'");
 	}
