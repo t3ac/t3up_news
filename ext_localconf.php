d601faaadd4e5f75f5ca6501f5322250ab1c8242
Michael Lang <113889180+t3ac@users.noreply.github.com>
2026-07-14 06:30:11 +0200

    Refactor ext_localconf.php to simplify configuration
    Removed unnecessary page TSConfig and TypoScript imports for eventnews.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,19 +1,8 @@
 <?php
 defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
-
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
+// Update flexforms
 call_user_func(static function () {
-	
-	// Update flexforms
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][] = \T3ac\T3upNews\Hooks\FlexFormHook::class;
-	
-	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/News/Templates.tsconfig'");
-	ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/News/Design.tsconfig'");
-
-	if (ExtensionManagementUtility::isLoaded('eventnews')) {
-       		ExtensionManagementUtility::addPageTSConfig("@import 'EXT:t3up_news/Configuration/TsConfig/Eventnews/Templates.tsconfig'");
-        	ExtensionManagementUtility::addTypoScriptSetup("@import 'EXT:t3up_news/Configuration/TypoScript/Eventnews/setup.typoscript'");
-		ExtensionManagementUtility::addTypoScriptConstants("@import 'EXT:t3up_news/Configuration/TypoScript/Eventnews/constants.typoscript'");
-	}
 });
